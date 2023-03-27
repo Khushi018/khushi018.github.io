9140735e9db8d23350baf7cf61c1f3ba7940a91a
Khushi Gupta <khushigupta.of@gmail.com>
2023-03-27 21:20:04 +0530

    ok

---
+++ port.js
@@ -0,0 +1,56 @@
+document.querySelector(".home").addEventListener("click",()=>{
+    document.querySelector("#home").scrollIntoView();
+  
+})
+
+document.querySelector(".about").addEventListener("click",()=>{
+   document.querySelector("#about").scrollIntoView();
+  
+})
+
+document.querySelector(".skills").addEventListener("click",()=>{
+   document.querySelector("#skills").scrollIntoView();
+  
+})
+
+document.querySelector(".projects").addEventListener("click",()=>{
+   document.querySelector("#projects").scrollIntoView();
+  
+})
+document.querySelector(".contact").addEventListener("click",()=>{
+   document.querySelector("#contact").scrollIntoView();
+  
+})
+
+document.querySelector("#resume-button-1").addEventListener("click",()=>{
+   window.open("https://drive.google.com/file/d/17Fk2CVW9-0xroEx3CnSDSI1h_uvX3caL/view?usp=share_link","_blank") ;
+})
+
+document.querySelector("#resume-button-2").addEventListener("click",()=>{
+  window.open("https://drive.google.com/file/d/17Fk2CVW9-0xroEx3CnSDSI1h_uvX3caL/view?usp=share_link","_blank") ;
+})
+
+
+function linked(){
+   window.open("https://www.linkedin.com/in/khushi-gupta-39b784225/","_blank") ;
+}
+
+function gith(){
+   window.open("https://github.com/Khushi018","_blank") ;
+} 
+
+function gitP1(){
+   window.open("https://github.com/Khushi018/MyGlamm-Clone-","_blank") ;
+}
+
+function depP1(){
+   window.open("https://hermoso.netlify.app/","_blank") ;
+}
+
+function gitP2(){
+   window.open("https://github.com/Khushi018/MyGlamm-Clone-","_blank") ;
+}
+
+function depP2(){
+   window.open("https://hermoso.netlify.app/","_blank") ;
+}
